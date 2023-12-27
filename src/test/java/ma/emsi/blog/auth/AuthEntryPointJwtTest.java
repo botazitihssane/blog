9995e34af3d406050e0cdfe09baa586473a96a7a
botazitihssane <botazitihssane@outlook.com>
2023-12-28 00:36:00 +0100

    test jwt

--- src/test/java/ma/emsi/blog/auth/AuthEntryPointJwtTest.java
+++ src/test/java/ma/emsi/blog/auth/AuthEntryPointJwtTest.java
@@ -3,11 +3,8 @@ package ma.emsi.blog.auth;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import static org.mockito.Mockito.*;
 
 import org.junit.Before;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -16,28 +13,26 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthEntryPointJwtTest {
 
-	@Mock
-	private AuthenticationException authenticationException;
-
 	private AuthEntryPointJwt authEntryPointJwt;
 	private MockHttpServletRequest request;
 	private MockHttpServletResponse response;
 
 	@Before
 	public void setUp() {
-		MockitoAnnotations.openMocks(this);
 		authEntryPointJwt = new AuthEntryPointJwt();
 		request = new MockHttpServletRequest();
 		response = new MockHttpServletResponse();
 	}
 
 	@Test
-	    public void testCommence() throws Exception {
-	        when(authenticationException.getMessage()).thenReturn("Unauthorized error");
+	public void testCommence() throws Exception {
+		AuthenticationException authenticationException = new AuthenticationException("Unauthorized error") {
+			private static final long serialVersionUID = 1L;
+		};
 
-	        authEntryPointJwt.commence(request, response, authenticationException);
+		authEntryPointJwt.commence(request, response, authenticationException);
 
-	        assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
-	        assertEquals("Error: Unauthorized", response.getErrorMessage());
-	    }
+		assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
+		assertEquals("Error: Unauthorized", response.getErrorMessage());
+	}
 }
