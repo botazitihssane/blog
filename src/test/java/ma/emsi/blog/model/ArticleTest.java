f9b8a6b78408da27403c636a6fe1e8875abee196
botazitihssane <botazitihssane@outlook.com>
2023-12-29 01:55:25 +0100

    test models

--- src/test/java/ma/emsi/blog/model/ArticleTest.java
+++ src/test/java/ma/emsi/blog/model/ArticleTest.java
@@ -9,6 +9,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
@@ -16,118 +20,154 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
+@RunWith(MockitoJUnitRunner.class)
+
 public class ArticleTest {
+
+	@InjectMocks
+	private Article article;
+
+	@Mock
+	private Categorie categorie;
+
+	@Mock
+	private Proprietaire proprietaire;
 
 	private Validator validator;
 
-	private Article article;
-
 	@Before
 	public void setUp() {
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		validator = factory.getValidator();
 		article = new Article();
 	}
 
 	@Test
-	public void testTitreNotBlank() {
-		article.setTitre("");
-		assertViolations("titre");
+	public void getId() {
+		article.setId(1);
+		assertEquals(1, article.getId());
 	}
 
 	@Test
-	public void testTexteNotBlank() {
-		article.setTexte("");
-		assertViolations("texte");
+	public void getTitre() {
+		article.setTitre("Test Title");
+		assertEquals("Test Title", article.getTitre());
 	}
 
 	@Test
-	public void testDateNotNull() {
-		article.setDate(null);
-		assertViolations("date");
+	public void setDate() {
+		LocalDate currentDate = LocalDate.now();
+		article.setDate(currentDate);
+		assertEquals(currentDate, article.getDate());
 	}
 
 	@Test
-	public void testNombreCommentaireMin() {
-		article.setNombreCommentaire(-1);
-		assertViolations("nombreCommentaire");
-	}
-
-	@Test
-	public void testCategorieNotNull() {
-		article.setCategorie(null);
-		assertViolations("categorie");
-	}
-
-	@Test
-	public void testUserNotNull() {
-		article.setUser(null);
-		assertViolations("user");
-	}
-
-	private void assertViolations(String fieldName) {
-		Set<ConstraintViolation<Article>> violations = validator.validateProperty(article, fieldName);
-		assertFalse("Expected validation errors for " + fieldName, violations.isEmpty());
-	}
-
-	@Test
-	public void testGettersAndSetters() {
-		int id = 1;
-		String titre = "Test Title";
-		String texte = "Test Text";
-		String photo = "Test Photo";
-		String lien = "Test Link";
-		LocalDate date = LocalDate.now();
-		int nombreCommentaire = 10;
-		Categorie categorie = new Categorie(); // Assuming you have a Categorie class
-		Proprietaire user = new Proprietaire(); // Assuming you have a Proprietaire class
-
-		article.setId(id);
-		article.setTitre(titre);
-		article.setTexte(texte);
-		article.setPhoto(photo);
-		article.setLien(lien);
-		article.setDate(date);
-		article.setNombreCommentaire(nombreCommentaire);
+	public void setCategorie() {
 		article.setCategorie(categorie);
-		article.setUser(user);
-
-		assertEquals(id, article.getId());
-		assertEquals(titre, article.getTitre());
-		assertEquals(texte, article.getTexte());
-		assertEquals(photo, article.getPhoto());
-		assertEquals(lien, article.getLien());
-		assertEquals(date, article.getDate());
-		assertEquals(nombreCommentaire, article.getNombreCommentaire());
 		assertEquals(categorie, article.getCategorie());
-		assertEquals(user, article.getUser());
 	}
 
 	@Test
-	public void testConstructorWithParameters() {
-		int id = 1;
-		String titre = "Test Title";
-		String texte = "Test Text";
-		String photo = "Test Photo";
-		String lien = "Test Link";
-		LocalDate date = LocalDate.now();
-		int nombreCommentaire = 10;
-		Categorie categorie = new Categorie();
-		Proprietaire user = new Proprietaire();
-
-		Article articleWithParams = new Article(id, titre, texte, photo, lien, date, nombreCommentaire, categorie,
-				user);
-
-		assertNotNull(articleWithParams);
-		assertEquals(id, articleWithParams.getId());
-		assertEquals(titre, articleWithParams.getTitre());
-		assertEquals(texte, articleWithParams.getTexte());
+	public void setUser() {
+		article.setUser(proprietaire);
+		assertEquals(proprietaire, article.getUser());
 	}
 
 	@Test
-	public void testNoArgsConstructor() {
-		Article newArticle = new Article();
-		assertNotNull(newArticle);
+	public void testConstructorAndSettersAndGetters() {
+		Article newArticle = new Article(1, "Title", "Text", "photo.jpg", "http://example.com", LocalDate.now(), 5,
+				categorie, proprietaire);
+
+		assertEquals(1, newArticle.getId());
+		assertEquals("Title", newArticle.getTitre());
+		assertEquals("Text", newArticle.getTexte());
+		assertEquals("photo.jpg", newArticle.getPhoto());
+		assertEquals("http://example.com", newArticle.getLien());
+		assertEquals(LocalDate.now(), newArticle.getDate());
+		assertEquals(5, newArticle.getNombreCommentaire());
+		assertEquals(categorie, newArticle.getCategorie());
+		assertEquals(proprietaire, newArticle.getUser());
+
+		// Test setters and getters
+		article.setId(2);
+		article.setTitre("New Title");
+		article.setTexte("New Text");
+		article.setPhoto("new_photo.jpg");
+		article.setLien("http://new-example.com");
+		article.setDate(LocalDate.now().plusDays(1));
+		article.setNombreCommentaire(10);
+		article.setCategorie(null);
+		article.setUser(null);
+
+		assertEquals(2, article.getId());
+		assertEquals("New Title", article.getTitre());
+		assertEquals("New Text", article.getTexte());
+		assertEquals("new_photo.jpg", article.getPhoto());
+		assertEquals("http://new-example.com", article.getLien());
+		assertEquals(LocalDate.now().plusDays(1), article.getDate());
+		assertEquals(10, article.getNombreCommentaire());
+		assertNull(article.getCategorie());
+		assertNull(article.getUser());
 	}
+
+	/*
+	 * @Test public void testTitreNotBlank() { article.setTitre("");
+	 * assertViolations("titre"); }
+	 * 
+	 * @Test public void testTexteNotBlank() { article.setTexte("");
+	 * assertViolations("texte"); }
+	 * 
+	 * @Test public void testDateNotNull() { article.setDate(null);
+	 * assertViolations("date"); }
+	 * 
+	 * @Test public void testNombreCommentaireMin() {
+	 * article.setNombreCommentaire(-1); assertViolations("nombreCommentaire"); }
+	 * 
+	 * @Test public void testCategorieNotNull() { article.setCategorie(null);
+	 * assertViolations("categorie"); }
+	 * 
+	 * @Test public void testUserNotNull() { article.setUser(null);
+	 * assertViolations("user"); }
+	 * 
+	 * private void assertViolations(String fieldName) {
+	 * Set<ConstraintViolation<Article>> violations =
+	 * validator.validateProperty(article, fieldName);
+	 * assertFalse("Expected validation errors for " + fieldName,
+	 * violations.isEmpty()); }
+	 * 
+	 * @Test public void gettersAndSetters() { int id = 1; String titre =
+	 * "Test Title"; String texte = "Test Text"; String photo = "Test Photo"; String
+	 * lien = "Test Link"; LocalDate date = LocalDate.now(); int nombreCommentaire =
+	 * 10; Categorie categorie = new Categorie(); // Assuming you have a Categorie
+	 * class Proprietaire user = new Proprietaire(); // Assuming you have a
+	 * Proprietaire class
+	 * 
+	 * article.setId(id); article.setTitre(titre); article.setTexte(texte);
+	 * article.setPhoto(photo); article.setLien(lien); article.setDate(date);
+	 * article.setNombreCommentaire(nombreCommentaire);
+	 * article.setCategorie(categorie); article.setUser(user);
+	 * 
+	 * assertEquals(id, article.getId()); assertEquals(titre, article.getTitre());
+	 * assertEquals(texte, article.getTexte()); assertEquals(photo,
+	 * article.getPhoto()); assertEquals(lien, article.getLien());
+	 * assertEquals(date, article.getDate()); assertEquals(nombreCommentaire,
+	 * article.getNombreCommentaire()); assertEquals(categorie,
+	 * article.getCategorie()); assertEquals(user, article.getUser()); }
+	 * 
+	 * @Test public void testConstructorWithParameters() { int id = 1; String titre
+	 * = "Test Title"; String texte = "Test Text"; String photo = "Test Photo";
+	 * String lien = "Test Link"; LocalDate date = LocalDate.now(); int
+	 * nombreCommentaire = 10; Categorie categorie = new Categorie(); Proprietaire
+	 * user = new Proprietaire();
+	 * 
+	 * Article articleWithParams = new Article(id, titre, texte, photo, lien, date,
+	 * nombreCommentaire, categorie, user);
+	 * 
+	 * assertNotNull(articleWithParams); assertEquals(id,
+	 * articleWithParams.getId()); assertEquals(titre,
+	 * articleWithParams.getTitre()); assertEquals(texte,
+	 * articleWithParams.getTexte()); }
+	 * 
+	 * @Test public void testNoArgsConstructor() { Article newArticle = new
+	 * Article(); assertNotNull(newArticle); }
+	 */
 
 }
--- src/test/java/ma/emsi/blog/model/CategorieTest.java
+++ src/test/java/ma/emsi/blog/model/CategorieTest.java
@@ -5,40 +5,67 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import jakarta.validation.ValidatorFactory;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ConstraintViolation;
+import java.util.Set;
+
 public class CategorieTest {
 
-	
+	private Categorie categorie;
 
-	    private Categorie categorie;
-
-	    @Before
-	    public void setUp() {
-	        categorie = new Categorie();
-	    }
-
-	    @Test
-	    public void testDefaultConstructor() {
-	        assertNotNull("Categorie instance should not be null", categorie);
-	    }
-
-	    @Test
-	    public void testParameterizedConstructor() {
-	        Categorie paramCategorie = new Categorie(1, "TestCategory");
-	        assertEquals("Id should be 1", 1, paramCategorie.getId());
-	        assertEquals("Nom should be 'TestCategory'", "TestCategory", paramCategorie.getNom());
-	    }
-
-	    @Test
-	    public void testSetAndGetId() {
-	        int expectedId = 10;
-	        categorie.setId(expectedId);
-	        assertEquals("Getter or setter for id not working", expectedId, categorie.getId());
-	    }
-
-	    @Test
-	    public void testSetAndGetNom() {
-	        String expectedNom = "NewCategory";
-	        categorie.setNom(expectedNom);
-	        assertEquals("Getter or setter for nom not working", expectedNom, categorie.getNom());
-	    }
+	@Before
+	public void setUp() {
+		categorie = new Categorie();
 	}
+
+	@Test
+	public void testBlankNomValidation() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		Categorie categorie = new Categorie();
+		categorie.setNom("");
+		Set<ConstraintViolation<Categorie>> violations = validator.validate(categorie);
+
+		assertFalse("Nom should not be valid", violations.isEmpty());
+		for (ConstraintViolation<Categorie> violation : violations) {
+			assertEquals("Le nom ne peut pas etre vide", violation.getMessage());
+		}
+	}
+
+	@Test
+	public void whenAllArgsConstructorUsed_thenPropertiesAreSet() {
+		int expectedId = 1;
+		String expectedNom = "Test Category";
+		Categorie categorie = new Categorie(expectedId, expectedNom);
+		assertEquals(expectedId, categorie.getId());
+		assertEquals(expectedNom, categorie.getNom());
+	}
+
+	@Test
+	public void testGetId() {
+		categorie.setId(1);
+		assertEquals(1, categorie.getId());
+	}
+
+	@Test
+	public void testSetId() {
+		categorie.setId(1);
+		assertEquals(1, categorie.getId());
+	}
+
+	@Test
+	public void testGetNom() {
+		categorie.setNom("Test Nom");
+		assertEquals("Test Nom", categorie.getNom());
+	}
+
+	@Test
+	public void testSetNom() {
+		categorie.setNom("Test Nom");
+		assertEquals("Test Nom", categorie.getNom());
+	}
+}
